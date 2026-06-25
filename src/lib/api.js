3f2404140c44ecbdc74ea316761a632e1d58f1c0
Nguyễn Xuân Khởi <alexuankhoi@gmail.com>
2026-06-25 22:10:40 +0700

    789

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const API_URL = import.meta.env.VITE_API_URL || 'https://api.dainam.site/gov';
+export const BASE_URL = API_URL;
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
